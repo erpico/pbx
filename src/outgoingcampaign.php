8e621cfc30bcfd208c9ae50571b4f573a013cfb3
CI Runner <runner@erpico.ru>
2022-03-14 10:16:27 +0000

    Merge branch 'APZ-18' into 'develop'
    Resolve APZ-18
    
    Closes APZ-18
    
    See merge request erpicopbx/www!183

--- src/outgoingcampaign.php
+++ src/outgoingcampaign.php
@@ -373,7 +373,7 @@ class PBXOutgoingCampaign  {
         if (strlen($ssql)) $ssql .= ",";
         $ssql .= "`".$field."`='".($isInt ? intval($values[$field]) : trim(addslashes($values[$field])))."'";          
       }  else {
-          if (($field == "lead_status" || $field == "lead_status_user" || $field == "call_tries_end") && $values[$field] == "") {
+          if (($field == "lead_status" || $field == "lead_status_user" || $field == "lead_status_tries_end") && $values[$field] == "") {
               if (strlen($ssql)) $ssql .= ",";
               $ssql .= "`".$field."`=null";
               continue;
